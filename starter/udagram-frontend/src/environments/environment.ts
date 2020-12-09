22c93c9f0b1672940ca8448598c52173056b4b86
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2020-12-09 17:56:13 -0500

    circle

--- starter/udagram-frontend/src/environments/environment.ts
+++ starter/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: "Udagram",
-  apiHost: "ec2-34-239-132-55.compute-1.amazonaws.com:8080/api/v0",
+  apiHost: "http://ec2-34-239-132-55.compute-1.amazonaws.com:8080/api/v0",
 };
 
 /*
