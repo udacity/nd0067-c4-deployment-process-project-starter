8f2ef83d56d96272bddf1555957131c2c670ee62
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2020-12-09 17:08:04 -0500

    stuff

--- starter/bin/deploy.sh
+++ starter/bin/deploy.sh
@@ -8,10 +8,12 @@ git pull
 pm2 kill
 
 # build and start api
+npm run api:install
 npm run api:build
 pm2 start npm --name "udagram-api" -- run api:prod 
 
 # build and start frontend 
+npm run frontend:install
 npm run frontend:build 
 pm2 start npm --name "udagram-frontend" -- run frontend:prod 
 
