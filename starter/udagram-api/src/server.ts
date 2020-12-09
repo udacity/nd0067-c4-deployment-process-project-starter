8cc1e6f4a90c4e12970f83eb9a2d2b81ab6624db
Guillaume Bibeau-Laviolette <g.bibeaulaviolette@gmail.com>
2020-12-09 11:00:15 -0500

    enable cors anywhere

--- starter/udagram-api/src/server.ts
+++ starter/udagram-api/src/server.ts
@@ -23,20 +23,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   app.use(bodyParser.json());
 
-  app.use(
-    cors({
-      allowedHeaders: [
-        "Origin",
-        "X-Requested-With",
-        "Content-Type",
-        "Accept",
-        "X-Access-Token",
-        "Authorization",
-      ],
-      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
-      origin: process.env.URL,
-    })
-  );
+  app.use(cors());
 
   app.use("/api/v0/", IndexRouter);
 
