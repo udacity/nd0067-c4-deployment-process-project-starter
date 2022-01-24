afbf71aa5280dd9f988d3c40b0077bbced004f81
katerina-tziala <katerina.tziala@gmail.com>
2022-01-24 23:12:25 +0100

    chore: update

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://udagramapi-env.eba-9uh4euee.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
