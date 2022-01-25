51c5b7b4ae4f758a34528e715cbf80c5d2babae9
katerina-tziala <katerina.tziala@gmail.com>
2022-01-25 22:08:33 +0100

    chore: update deploy

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,6 +1,6 @@
-echo 'test'
+echo 'deploy api'
 # eb init --region $AWS_DEFAULT_REGION $EB_APP
 # eb deploy $EB_ENV
 eb init --region us-east-1 udagram-api
-eb deploy Udagramapi-env
-# eb deploy
+eb use Udagramapi-env
+eb deploy
