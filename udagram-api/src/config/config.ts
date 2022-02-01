3ecf3c467b32b024b8436f073e8e44cf646d3004
choiyounyeong <young0770@naver.com>
2022-02-01 14:42:44 +0900

    Organized folder

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,13 +1,14 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
+  db_port: Number(process.env.DB_PORT),
   port: Number(process.env.PORT),
   host: process.env.POSTGRES_HOST,
-  dialect: "postgres",
+  dialect: 'postgres',
   aws_region: process.env.AWS_REGION,
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
