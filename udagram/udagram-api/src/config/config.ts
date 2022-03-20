1adb37cb65a39b239b88c8fbd7834589d04cd41d
andrewkadry <andrew.raghib@gmail.com>
2022-03-20 11:29:20 +0200

    h

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -5,7 +5,7 @@ export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  port: Number(process.env.PORT),
+  port: Number(process.env.POSTGRES_PORT),
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -28,6 +28,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
+    res.status(200)
     res.send("/api/v0/");
   });
 
