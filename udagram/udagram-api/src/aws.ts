ab2de937d9e7701145ae844cec01a5aee3ffb696
Ahmed basset <aabdulbasset5@gmail.com>
2022-04-09 12:41:22 -0700

    Update aws.ts
    [bug] removed sharedinifilecredentials method: not needed

--- udagram/udagram-api/src/aws.ts
+++ udagram/udagram-api/src/aws.ts
@@ -1,10 +1,6 @@
 import AWS = require("aws-sdk");
 import { config } from "./config/config";
 
-// Configure AWS
-const credentials = new AWS.SharedIniFileCredentials({ profile: "default" });
-AWS.config.credentials = credentials;
-
 export const s3 = new AWS.S3({
   signatureVersion: "v4",
   region: config.aws_region,
