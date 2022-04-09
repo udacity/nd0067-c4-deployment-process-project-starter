d6b3bc76f2988fce9d3623c1b42e60be577fff33
Ahmed basset <aabdulbasset5@gmail.com>
2022-04-09 12:49:05 -0700

    Update aws.ts
    Clearified the remove of SharedIniFileCredentials method

--- udagram/udagram-api/src/aws.ts
+++ udagram/udagram-api/src/aws.ts
@@ -1,6 +1,8 @@
 import AWS = require("aws-sdk");
 import { config } from "./config/config";
 
+//Credentials are auto set according to the documentation https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/setting-credentials-node.html and the default profile is "Default anyway"
+
 export const s3 = new AWS.S3({
   signatureVersion: "v4",
   region: config.aws_region,
