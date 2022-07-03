a42c6865342d80cccbd7b63a454efa45ae8f20a0
Hazem <hazemzon@gmail.com>
2022-07-03 22:27:28 +0200

    eretrg

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -11,6 +11,7 @@ export const config = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   port: Number(process.env.PORT),
+  dbPort: Number(process.env.DB_PORT),
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
   aws_region: process.env.AWS_REGION,
