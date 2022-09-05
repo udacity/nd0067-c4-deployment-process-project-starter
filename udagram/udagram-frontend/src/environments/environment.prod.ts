e06fc8a982880e5cb194977c7764ddee04c340f1
Hesham Abdelhafez <hesham3la2@gmail.com>
2022-09-05 07:26:54 +0300

    changes

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://localhost:8080/api/v0',
 };
 
 /*
