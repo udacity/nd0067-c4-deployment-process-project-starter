3bd76db4010158119c964bce388fc7d52623a345
hebaahmed400@gmail.com <hebaahmed400@gmail.com>
2022-12-10 00:37:29 +0200

    add updates

--- udagram/udagram-frontend/src/app/feed/services/feed.provider.service.ts
+++ udagram/udagram-frontend/src/app/feed/services/feed.provider.service.ts
@@ -5,25 +5,29 @@ import { BehaviorSubject } from 'rxjs';
 import { ApiService } from '../../api/api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FeedProviderService {
-  currentFeed$: BehaviorSubject<FeedItem[]> = new BehaviorSubject<FeedItem[]>([]);
+  currentFeed$: BehaviorSubject<FeedItem[]> = new BehaviorSubject<FeedItem[]>(
+    []
+  );
 
-  constructor(private api: ApiService) { }
+  constructor(private api: ApiService) {}
 
   async getFeed(): Promise<BehaviorSubject<FeedItem[]>> {
     const req = await this.api.get('/feed');
-    const items = <FeedItem[]> req.rows;
+    const items = <FeedItem[]>req.rows;
     this.currentFeed$.next(items);
     return Promise.resolve(this.currentFeed$);
   }
 
   async uploadFeedItem(caption: string, file: File): Promise<any> {
-    const res = await this.api.upload('/feed', file, {caption: caption, url: file.name});
+    const res = await this.api.upload('/feed', file, {
+      caption: caption,
+      url: file.name,
+    });
     const feed = [res, ...this.currentFeed$.value];
     this.currentFeed$.next(feed);
     return res;
   }
-
 }
--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: 'Udagram',
+  apiHost: 'http://localhost:8080/api/v0',
 };
 
 /*
