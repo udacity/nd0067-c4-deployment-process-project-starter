138bf543106682b8fb4ddfb69be5d550f19e19ee
dRichards188 <github@cxoprotect.com>
2022-12-16 11:06:16 -0700

    altering for conversion to elastic beanstalk

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  apiHost: "udagram-api-dev2222.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
