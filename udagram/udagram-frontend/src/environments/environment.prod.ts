b9edb84399b801636c7859f0728c8bacb1ea1c05
dRichards188 <github@cxoprotect.com>
2022-12-16 11:51:39 -0700

    resolving linting errors

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "udagram-api-dev2222.us-east-1.elasticbeanstalk.com/api/v0",
+  appName: 'Udagram',
+  apiHost: 'udagram-api-dev2222.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
