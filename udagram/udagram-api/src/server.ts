f61afc66d0904e21fc76292224cef779a8ed1ac4
Amin Wassef <dr.amin.wassef@gmail.com>
2023-02-08 23:20:14 +0300

    Server.ts up.

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -25,7 +25,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = process.env.EB_URL || 8080;
+  const port = process.env.PORT;
 
   app.use(bodyParser.json());
 
