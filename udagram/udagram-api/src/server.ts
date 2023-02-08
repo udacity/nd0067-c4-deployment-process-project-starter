4f60040f51edba31b0f66e60c6862c4cead5c6a4
Amin Wassef <dr.amin.wassef@gmail.com>
2023-02-08 09:23:55 +0300

    Deployment Health: Ok

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -25,7 +25,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   console.log("Database Connected");
 
   const app = express();
-  const port = 8080;
+  const port = process.env.EB_URL || 8080;
 
   app.use(bodyParser.json());
 
