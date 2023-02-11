a6501c04e0baa86ce6295c9e85f7d6c3c543ff1b
Amin Wassef <dr.amin.wassef@gmail.com>
2023-02-11 06:49:20 +0300

    Change api Welcome

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -49,7 +49,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
-    res.send("/api/v0/");
+    res.send("Hellow from circleci effect");
   });
 
   // Start the Server
